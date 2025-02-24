69de11d848623f2f79d803cafd69b4a22754c378
Hacker God <jyrone.parker@gmail.com>
2025-02-23 22:00:41 -0500

    Add navigation

--- Views/StartScreen.swift
+++ Views/StartScreen.swift
@@ -1,49 +1,35 @@
-//
-//  SwiftUIView.swift
-//  FindTheBall
-//
-//  Created by Kennedy Pryor on 2/23/25.
-//
-
 import SwiftUI
-
 
 struct StartScreen: View {
     var body: some View {
-        VStack {
-            
-            Text("Find the Ball")
-                .font(.title)
-                .fontWeight(.semibold)
-                .padding(.top)
-                .foregroundStyle(.pink)
-            
-            
-            
-            Text("Time for an AR Scavenger Hunt!")
-                .font(.title2)
-                .foregroundStyle(.purple)
-            
-            
-            
-            
-            Button {
-                print("Let's Go!")
-            } label: {
-                Text("Let's Go!")
-                    .font(.title3)
-                    .foregroundColor(.white)
-                    .padding()
-                    .background(Color.blue)
-                    .cornerRadius(20)
-            }
-                    
-        }
-        
-        .padding()
+        NavigationStack {
+            VStack {
                 
+                Text("Find the Ball")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .padding(.top)
+                    .foregroundStyle(.pink)
+                
+                Text("Time for an AR Scavenger Hunt!")
+                    .font(.title2)
+                    .foregroundStyle(.purple)
+                
+                NavigationLink(destination: ContentView()) {
+                    Text("Let's Go!")
+                        .font(.title3)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(20)
+                }
+                
+            }
+            .padding()
+        }
     }
 }
+
 
 #Preview {
     StartScreen()
